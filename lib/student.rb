53e6c30d8f61aa22e6bb9044cdf00c48255cfbbc
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-30 02:23:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,6 +71,19 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      limit 1
+    SQL
+
+    DB[:conn].execute(sql,x).map do |row|
+      find_by_name(row[1])
+    end
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
